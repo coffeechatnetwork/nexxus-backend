dea2c111f9ec34c9b027cf1d3dad4ef12413a2bc
jun-nexxus <jiang.jun@nexxus.io>
2026-03-02 20:22:29 +0800

    feat: add interceptor to handle org_id.

--- nexxus-server/src/main/java/com/nexxus/server/config/MybatisPlusConfig.java
+++ nexxus-server/src/main/java/com/nexxus/server/config/MybatisPlusConfig.java
@@ -1,7 +1,13 @@
 package com.nexxus.server.config;
 
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.handler.TenantLineHandler;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.TenantLineInnerInterceptor;
+import com.nexxus.common.AccountInfo;
+import com.nexxus.common.AccountInfoContext;
+import net.sf.jsqlparser.expression.Expression;
+import net.sf.jsqlparser.expression.StringValue;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,8 +18,29 @@ public class MybatisPlusConfig {
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
 
+        TenantLineInnerInterceptor tenantInterceptor = new TenantLineInnerInterceptor(new TenantLineHandler() {
+            @Override
+            public Expression getTenantId() {
+                AccountInfo accountInfo = AccountInfoContext.get();
+                return new StringValue(String.valueOf(accountInfo.getOrgId()));
+            }
+
+            @Override
+            public String getTenantIdColumn() {
+                return "org_id";
+            }
+
+            @Override
+            public boolean ignoreTable(String tableName) {
+                return tableName.equals("cos_comment") ||
+                        tableName.equals("cos_organization");
+            }
+        });
+
         PaginationInnerInterceptor pagination = new PaginationInnerInterceptor();
         pagination.setMaxLimit(200L);
+
+        interceptor.addInnerInterceptor(tenantInterceptor);
         interceptor.addInnerInterceptor(pagination);
 
         return interceptor;
