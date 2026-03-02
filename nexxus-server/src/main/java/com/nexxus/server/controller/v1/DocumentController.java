85ecd3644b62a709e84b0cd337fc5658d52fc543
jun-nexxus <jiang.jun@nexxus.io>
2026-03-02 11:12:29 +0800

    feat: add document controller

---
+++ nexxus-server/src/main/java/com/nexxus/server/controller/v1/DocumentController.java
@@ -0,0 +1,23 @@
+package com.nexxus.server.controller.v1;
+
+import com.nexxus.cos.api.dto.document.CreateDocumentFolderRequest;
+import com.nexxus.cos.api.dto.document.DocumentFolderDto;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/v1/documents")
+@RequiredArgsConstructor
+public class DocumentController {
+
+    @PostMapping("/folders")
+    public DocumentFolderDto createFolder(@RequestBody @Valid CreateDocumentFolderRequest req) {
+        return null;
+    }
+}
